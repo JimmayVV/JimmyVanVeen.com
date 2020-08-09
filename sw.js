2228e3d3a36b3640e536b1e9854947d0ca1d1e42
Jimmy Van Veen <jimmy.van.veen@gmail.com>
2020-08-09 08:17:26 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c01eeb566d3be136d16f.js"
+    "url": "webpack-runtime-0c8d96683556055b8425.js"
   },
   {
     "url": "styles.f49630f2357441294871.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "18dcfbc80801b782261f21d283e7967f"
+    "revision": "390a8851b5b048c97eedde4ed7d4ab53"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e36cbe850dd4d52ef5a6.js"
